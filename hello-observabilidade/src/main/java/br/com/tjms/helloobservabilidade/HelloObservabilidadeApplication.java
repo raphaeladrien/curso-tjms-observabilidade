72b2dc41eb520b19467068ffe8225be2981fc398
radrien <raphael.adrien@daimler.com>
2023-11-29 08:25:10 +0000

    Grafana + Prometheus + spring base application

---
+++ hello-observabilidade/src/main/java/br/com/tjms/helloobservabilidade/HelloObservabilidadeApplication.java
@@ -0,0 +1,13 @@
+package br.com.tjms.helloobservabilidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloObservabilidadeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloObservabilidadeApplication.class, args);
+    }
+
+}
---
+++ hello-observabilidade/src/test/java/br/com/tjms/helloobservabilidade/HelloObservabilidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tjms.helloobservabilidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloObservabilidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
